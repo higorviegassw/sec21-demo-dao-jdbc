e3186fc991da362242b7b7fc066317a22a6de10b
Higor Viegas <viegas.higor@gmail.com>
2020-08-17 15:16:25 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,13 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao creteSellerDao() {
 		
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
